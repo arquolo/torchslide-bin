10f0747ee6815d9fcd19647e7b4bbabd5541e465
Paul M <arquolo@gmail.com>
2019-10-09 19:46:29 +0500

    Bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ else:
 
 setuptools.setup(
     name='torchslide',
-    version='0.2.0',
+    version='0.3.0',
     url='https://github.com/arquolo/torchslide-bin',
     author='Paul Maevskikh',
     author_email='arquolo@gmail.com',
